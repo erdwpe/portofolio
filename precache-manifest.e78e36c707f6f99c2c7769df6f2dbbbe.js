a7bac5b3c6eb04a9dd089af679d7905d7e894d74
erdwpe <erdwpe@gmail.com>
2023-06-21 15:16:44 +0700

    Updates

---
+++ precache-manifest.e78e36c707f6f99c2c7769df6f2dbbbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5018a338f8f87257ac4cccb6cee479",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "055c66a2995539b31bb5",
+    "url": "/portofolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "9bfb618d3cdcbfeab0be",
+    "url": "/portofolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "055c66a2995539b31bb5",
+    "url": "/portofolio/static/js/2.bbe19578.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/portofolio/static/js/2.bbe19578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfb618d3cdcbfeab0be",
+    "url": "/portofolio/static/js/main.3b24175e.chunk.js"
+  },
+  {
+    "revision": "eea94cf5cce134e17a89",
+    "url": "/portofolio/static/js/runtime-main.d2361bd8.js"
+  }
+]);
